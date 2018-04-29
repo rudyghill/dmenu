214a0d749aa775f85b1b8f6d9ada432e3ab7919c
rudyghill <rudyghill@gmail.com>
2018-04-29 12:37:28 -0600

    On branch master  Initial commit  Changes to be committed: 	new file:   LICENSE 	new file:   Makefile 	new file:   README 	new file:   arg.h 	new file:   config.def.h 	new file:   config.h 	new file:   config.mk 	new file:   dmenu 	new file:   dmenu-center-4.8.diff 	new file:   dmenu.1 	new file:   dmenu.c 	new file:   dmenu.o 	new file:   dmenu_path 	new file:   dmenu_run 	new file:   drw.c 	new file:   drw.h 	new file:   drw.o 	new file:   stest 	new file:   stest.1 	new file:   stest.c 	new file:   stest.o 	new file:   util.c 	new file:   util.h 	new file:   util.o

---
+++ config.def.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=24"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#268BD2", "#000000" },
+	[SchemeSel] = { "#000000", "#268BD2" },
+	[SchemeOut] = { "#000000", "#268BD2" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines = 7;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
