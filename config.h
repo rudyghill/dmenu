56de318a174cef438714ab0395388307006d710b
Rudy Hill <rudyghill@gmail.com>
2018-08-05 22:01:20 -0600

    On branch master  Changes to be committed: 	modified:   config.def.h 	modified:   config.h 	deleted:    dmenu 	deleted:    dmenu.o 	deleted:    drw.o 	deleted:    stest 	deleted:    stest.o 	deleted:    util.o

--- config.h
+++ config.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=24"
+	"Inconsolata:pixelsize=36:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#859900", "#000000" },
-	[SchemeSel] = { "#000000", "#859900" },
-	[SchemeOut] = { "#000000", "#859900" },
+	[SchemeNorm] = { "#CB4B16", "#000000" },
+	[SchemeSel] = { "#000000", "#CB4B16" },
+	[SchemeOut] = { "#000000", "#CB4B16" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 7;
