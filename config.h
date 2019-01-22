ff0f0ab7d9122ce310f412ab3373a8190d21480b
Rudy Hill <rudyghill@gmail.com>
2019-01-21 20:13:01 -0700

    On branch master  Your branch is ahead of 'origin/master' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	solarized:   config.h

--- config.h
+++ config.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#fbf1c7", "#1d2021" },
-	[SchemeSel] = { "#1d2021", "#fbf1c7" },
-	[SchemeOut] = { "#1d2021", "#fbf1c7" },
+	[SchemeNorm] = { "#93a1a1", "#000000" },
+	[SchemeSel] = { "#000000", "#268bd2" },
+	[SchemeOut] = { "#000000", "#268bd2" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 7;
