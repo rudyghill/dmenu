039a682d28a4d85a7a25788f4438d793d5ed4eae
Rudy Hill <rudyghill@gmail.com>
2019-01-28 11:57:28 -0700

    no center

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Inconsolata:pixelsize=36:antialias=true:autohint=true"
+	"mono:size=12:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -14,7 +14,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#268bd2" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines = 7;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
