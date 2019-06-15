5c0e70e39551d88c8008e7643f0815d400766771
Rudy Hill <rudyghill@gmail.com>
2019-06-15 15:17:20 -0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified colors:   config.h

--- config.h
+++ config.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#93a1a1", "#000000" },
-	[SchemeSel] = { "#000000", "#268bd2" },
-	[SchemeOut] = { "#000000", "#268bd2" },
+	[SchemeNorm] = { "#cccccc", "#000000" },
+	[SchemeSel] = { "#ffffff", "#268bd2" },
+	[SchemeOut] = { "#ffffff", "#268bd2" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
